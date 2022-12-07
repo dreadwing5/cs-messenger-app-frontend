3d0d698ca4daae8e36a051f245d2e010c3dfcc4d
dreadwing5 <sachinspyk@outlook.com>
2022-12-07 12:28:08 +0530

    Initializing Repository

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 
 import Login from './pages/login/Login';
-import Register from './pages/register/Register';
 import DashBoard from './pages/dashboard/Dashboard';
 import Messenger from './pages/messenger/Messenger';
 
 function App() {
   const { user } = useContext(AuthContext);
   const redirect = (user) => {
-    return user.role === 'agent' ? (
-      <Navigate replace to={'/'} />
-    ) : (
-      <Navigate replace to={'/messenger'} />
-    );
+    if (user.role === 'agent') return <DashBoard />;
+    return <Messenger />;
   };
 
   return (
     <Routes>
-      <Route exact path='/' element={user ? <DashBoard /> : <Login />} />
+      <Route
+        exact
+        path='/'
+        element={user ? redirect(user.data.user) : <Login />}
+      />
       <Route
         path='/login'
         element={user ? redirect(user.data.user) : <Login />}
